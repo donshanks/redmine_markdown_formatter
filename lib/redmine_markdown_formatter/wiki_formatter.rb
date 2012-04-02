7d6fdbd2765836d477ec0ab41a0c9c7dcb49b5ec
Don Shanks <dshanks@marchex.com>
2012-04-02 10:20:34 -0700

    Added coderay syntax highlighting for the markdown formatter, still need to put in the require for plugin initial load

--- lib/redmine_markdown_formatter/wiki_formatter.rb
+++ lib/redmine_markdown_formatter/wiki_formatter.rb
@@ -1,4 +1,5 @@
 require 'rdiscount'
+require 'coderay'
 
 module RedmineMarkdownFormatter
   class WikiFormatter
@@ -7,13 +8,23 @@ module RedmineMarkdownFormatter
     end
 
     def to_html(&block)
-      html = RDiscount.new(@text, :smart).to_html
+      html = markdown( coderay(@text) )
       html.gsub(/<a\s/, "<a class='external'") # Add the `external` class to every link
     rescue => e
       return("<pre>problem parsing wiki text: #{e.message}\n"+
              "original text: \n"+
              @text+
              "</pre>")
+    end
+
+    def markdown( text )
+      RDiscount.new(text, :smart).to_html
+    end
+
+    def coderay( text )
+      text.gsub(/\<code( lang="(.+?)")?\>(.+?)\<\/code\>/m) do
+        CodeRay.scan($3.strip, $2).div(:line_numbers => :inline, :line_number_anchors => false, :wrap => :span)
+      end
     end
   end
 end
